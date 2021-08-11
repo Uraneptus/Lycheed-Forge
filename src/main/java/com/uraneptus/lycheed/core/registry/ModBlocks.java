7c0821cbc4597f6683d2e9991e1903cc6d08f3be
Uraneptus <50745606+Uraneptus333@users.noreply.github.com>
2021-08-11 19:05:56 +0200

    basic woodset + Logo by Nivolas

--- src/main/java/com/uraneptus/lycheed/core/registry/ModBlocks.java
+++ src/main/java/com/uraneptus/lycheed/core/registry/ModBlocks.java
@@ -3,10 +3,7 @@ package com.uraneptus.lycheed.core.registry;
 import com.uraneptus.lycheed.LycheedMod;
 import com.uraneptus.lycheed.common.blocks.ModLogBlock;
 import com.uraneptus.lycheed.common.blocks.ModWoodBlock;
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
-import net.minecraft.block.RotatedPillarBlock;
-import net.minecraft.block.SoundType;
+import net.minecraft.block.*;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
 import net.minecraftforge.common.ToolType;
@@ -17,25 +14,54 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, LycheedMod.MOD_ID);
 
-    public static AbstractBlock.Properties LYCHEE_WOOD_PROPERTIES() {
-        return AbstractBlock.Properties.of(Material.WOOD, MaterialColor.COLOR_RED).harvestTool(ToolType.AXE).strength(2.0F, 3.0F).sound(SoundType.WOOD);
-    }
+    public static final AbstractBlock.Properties LYCHEE_PLANKS_PROPERTIES = AbstractBlock.Properties.of(Material.WOOD, MaterialColor.COLOR_RED).harvestTool(ToolType.AXE).strength(2.0F, 3.0F).sound(SoundType.WOOD);
+    public static final AbstractBlock.Properties LYCHEE_LOG_PROPERTIES = AbstractBlock.Properties.of(Material.WOOD, MaterialColor.COLOR_RED).strength(2.0F).sound(SoundType.WOOD);
+    public static final AbstractBlock.Properties LYCHEE_DOOR_PROPERTIES = AbstractBlock.Properties.of(Material.WOOD, MaterialColor.COLOR_RED).strength(3.0F).sound(SoundType.WOOD);
+    public static final AbstractBlock.Properties LYCHEE_BUTTON_PROPERTIES = AbstractBlock.Properties.of(Material.WOOD, MaterialColor.COLOR_RED).strength(0.5F).sound(SoundType.WOOD);
+    public static final AbstractBlock.Properties LYCHEE_PRESSURE_PLATE_PROPERTIES = AbstractBlock.Properties.of(Material.WOOD, MaterialColor.COLOR_RED).strength(0.5F).sound(SoundType.WOOD);
+
+
+
 
 
     public static final RegistryObject<Block> LYCHEE_PLANKS = BLOCKS.register("lychee_planks",
-        () -> new Block(LYCHEE_WOOD_PROPERTIES()));
+        () -> new Block(LYCHEE_PLANKS_PROPERTIES));
 
     public static final RegistryObject<Block> STRIPPED_LYCHEE_LOG = BLOCKS.register("stripped_lychee_log",
-            () -> new RotatedPillarBlock(LYCHEE_WOOD_PROPERTIES()));
+            () -> new RotatedPillarBlock(LYCHEE_LOG_PROPERTIES));
 
     public static final RegistryObject<Block> STRIPPED_LYCHEE_WOOD = BLOCKS.register("stripped_lychee_wood",
-            () -> new RotatedPillarBlock(LYCHEE_WOOD_PROPERTIES()));
+            () -> new RotatedPillarBlock(LYCHEE_LOG_PROPERTIES));
 
     public static final RegistryObject<Block> LYCHEE_LOG = BLOCKS.register("lychee_log",
-            () -> new ModLogBlock(STRIPPED_LYCHEE_LOG, LYCHEE_WOOD_PROPERTIES()));
+            () -> new ModLogBlock(STRIPPED_LYCHEE_LOG, LYCHEE_LOG_PROPERTIES));
 
     public static final RegistryObject<Block> LYCHEE_WOOD = BLOCKS.register("lychee_wood",
-            () -> new ModWoodBlock(STRIPPED_LYCHEE_WOOD, LYCHEE_WOOD_PROPERTIES()));
+            () -> new ModWoodBlock(STRIPPED_LYCHEE_WOOD, LYCHEE_LOG_PROPERTIES));
+
+    public static final RegistryObject<Block> LYCHEE_SLAB = BLOCKS.register("lychee_slab",
+            () -> new SlabBlock(LYCHEE_PLANKS_PROPERTIES));
+
+    public static final RegistryObject<Block> LYCHEE_STAIRS = BLOCKS.register("lychee_stairs",
+            () -> new StairsBlock(LYCHEE_PLANKS.get().defaultBlockState(), LYCHEE_PLANKS_PROPERTIES));
+
+    public static final RegistryObject<Block> LYCHEE_DOOR = BLOCKS.register("lychee_door", ///
+            () -> new DoorBlock(LYCHEE_DOOR_PROPERTIES));
+
+    public static final RegistryObject<Block> LYCHEE_TRAPDOOR = BLOCKS.register("lychee_trapdoor",
+            () -> new TrapDoorBlock(LYCHEE_DOOR_PROPERTIES));
+
+    public static final RegistryObject<Block> LYCHEE_FENCE = BLOCKS.register("lychee_fence",
+            () -> new FenceBlock(LYCHEE_PLANKS_PROPERTIES));
+
+    public static final RegistryObject<Block> LYCHEE_FENCE_GATE = BLOCKS.register("lychee_fence_gate",
+            () -> new FenceGateBlock(LYCHEE_PLANKS_PROPERTIES));
+
+    public static final RegistryObject<Block> LYCHEE_PRESSURE_PLATE = BLOCKS.register("lychee_pressure_plate",
+            () -> new PressurePlateBlock(PressurePlateBlock.Sensitivity.EVERYTHING, LYCHEE_PRESSURE_PLATE_PROPERTIES));
+
+    public static final RegistryObject<Block> LYCHEE_BUTTON = BLOCKS.register("lychee_button",
+            () -> new WoodButtonBlock(LYCHEE_BUTTON_PROPERTIES));
 
 
 
--- src/main/java/com/uraneptus/lycheed/core/registry/ModItems.java
+++ src/main/java/com/uraneptus/lycheed/core/registry/ModItems.java
@@ -14,36 +14,62 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, LycheedMod.MOD_ID);
 
-    public static Item.Properties buildingBlocks() {
-        return new Item.Properties().tab(ItemGroup.TAB_BUILDING_BLOCKS);
-    }
+    public static final Item.Properties BUILDING_BLOCKS = new Item.Properties().tab(ItemGroup.TAB_BUILDING_BLOCKS);
+    public static final Item.Properties DECORATION_BLOCKS = new Item.Properties().tab(ItemGroup.TAB_DECORATIONS);
+    public static final Item.Properties FOOD = new Item.Properties().tab(ItemGroup.TAB_FOOD);
+    public static final Item.Properties REDSTONE = new Item.Properties().tab(ItemGroup.TAB_REDSTONE);
+    public static final Item.Properties TRANSPORTATION = new Item.Properties().tab(ItemGroup.TAB_TRANSPORTATION);
 
     //FOOD
     public static final RegistryObject<Item> LYCHEE = ITEMS.register("lychee",
-            () -> new Item(new Item.Properties().tab(ItemGroup.TAB_FOOD)
-                    .food(new Food.Builder().nutrition(5).saturationMod(0.9F).build())));
+            () -> new Item(FOOD.food(new Food.Builder().nutrition(5).saturationMod(0.9F).build())));
 
     public static final RegistryObject<Item> ROTTEN_LYCHEE = ITEMS.register("rotten_lychee",
-            () -> new Item(new Item.Properties().tab(ItemGroup.TAB_FOOD)
-                    .food(new Food.Builder().nutrition(2).saturationMod(0.4F)
+            () -> new Item(FOOD.food(new Food.Builder().nutrition(2).saturationMod(0.4F)
                             .effect(() -> new EffectInstance(Effects.CONFUSION, 100, 1), 1.0F).build())));
 
 
     //BLOCK ITEMS
     public static final RegistryObject<BlockItem> LYCHEE_PLANKS = ITEMS.register("lychee_planks",
-            ()-> new BlockItem(ModBlocks.LYCHEE_PLANKS.get(), buildingBlocks()));
+            ()-> new BlockItem(ModBlocks.LYCHEE_PLANKS.get(), BUILDING_BLOCKS));
 
     public static final RegistryObject<BlockItem> LYCHEE_LOG = ITEMS.register("lychee_log",
-            ()-> new BlockItem(ModBlocks.LYCHEE_LOG.get(), buildingBlocks()));
+            ()-> new BlockItem(ModBlocks.LYCHEE_LOG.get(), BUILDING_BLOCKS));
 
     public static final RegistryObject<BlockItem> LYCHEE_WOOD = ITEMS.register("lychee_wood",
-            ()-> new BlockItem(ModBlocks.LYCHEE_WOOD.get(), buildingBlocks()));
+            ()-> new BlockItem(ModBlocks.LYCHEE_WOOD.get(), BUILDING_BLOCKS));
 
     public static final RegistryObject<BlockItem> STRIPPED_LYCHEE_LOG = ITEMS.register("stripped_lychee_log",
-            ()-> new BlockItem(ModBlocks.STRIPPED_LYCHEE_LOG.get(), buildingBlocks()));
+            ()-> new BlockItem(ModBlocks.STRIPPED_LYCHEE_LOG.get(), BUILDING_BLOCKS));
 
     public static final RegistryObject<BlockItem> STRIPPED_LYCHEE_WOOD = ITEMS.register("stripped_lychee_wood",
-            ()-> new BlockItem(ModBlocks.STRIPPED_LYCHEE_WOOD.get(), buildingBlocks()));
+            ()-> new BlockItem(ModBlocks.STRIPPED_LYCHEE_WOOD.get(), BUILDING_BLOCKS));
+
+    public static final RegistryObject<BlockItem> LYCHEE_SLAB = ITEMS.register("lychee_slab",
+            ()-> new BlockItem(ModBlocks.LYCHEE_SLAB.get(), BUILDING_BLOCKS));
+
+    public static final RegistryObject<BlockItem> LYCHEE_STAIRS = ITEMS.register("lychee_stairs",
+            ()-> new BlockItem(ModBlocks.LYCHEE_STAIRS.get(), BUILDING_BLOCKS));
+
+    public static final RegistryObject<BlockItem> LYCHEE_DOOR = ITEMS.register("lychee_door",
+            ()-> new BlockItem(ModBlocks.LYCHEE_DOOR.get(), REDSTONE));
+
+    public static final RegistryObject<BlockItem> LYCHEE_TRAPDOOR = ITEMS.register("lychee_trapdoor",
+            ()-> new BlockItem(ModBlocks.LYCHEE_TRAPDOOR.get(), REDSTONE));
+
+    public static final RegistryObject<BlockItem> LYCHEE_FENCE = ITEMS.register("lychee_fence",
+            ()-> new BlockItem(ModBlocks.LYCHEE_FENCE.get(), DECORATION_BLOCKS));
+
+    public static final RegistryObject<BlockItem> LYCHEE_FENCE_GATE = ITEMS.register("lychee_fence_gate",
+            ()-> new BlockItem(ModBlocks.LYCHEE_FENCE_GATE.get(), REDSTONE));
+
+    public static final RegistryObject<BlockItem> LYCHEE_PRESSURE_PLATE = ITEMS.register("lychee_pressure_plate",
+            ()-> new BlockItem(ModBlocks.LYCHEE_PRESSURE_PLATE.get(), REDSTONE));
+
+    public static final RegistryObject<BlockItem> LYCHEE_BUTTON = ITEMS.register("lychee_button",
+            ()-> new BlockItem(ModBlocks.LYCHEE_BUTTON.get(), REDSTONE));
+
+
 
 
 
