2ece59f11e3b2c2778c1982fb3d9c9530cd95492
Uraneptus <uraneptus33@gmail.com>
2022-03-27 19:50:26 +0200

    treegen improvements

--- src/main/java/com/uraneptus/lycheed/core/world/gen/feature/LycheeTreeSmall.java
+++ src/main/java/com/uraneptus/lycheed/core/world/gen/feature/LycheeTreeSmall.java
@@ -5,14 +5,16 @@ import com.teamabnormals.blueprint.core.util.TreeUtil;
 import com.uraneptus.lycheed.core.registry.ModBlocks;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.util.random.SimpleWeightedRandomList;
 import net.minecraft.world.level.WorldGenLevel;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.LeavesBlock;
-import net.minecraft.world.level.block.SaplingBlock;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
+import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
+import net.minecraft.world.level.levelgen.feature.stateproviders.SimpleStateProvider;
+import net.minecraft.world.level.levelgen.feature.stateproviders.WeightedStateProvider;
 
 import java.util.*;
 
@@ -20,6 +22,7 @@ public class LycheeTreeSmall extends Feature<NoneFeatureConfiguration> {
     private static final BlockState LOG = ModBlocks.LYCHEE_LOG.get().defaultBlockState();
     private static final BlockState LEAVES = ModBlocks.LYCHEE_LEAVES.get().defaultBlockState().setValue(LeavesBlock.DISTANCE, 2);
     private static final BlockState FRUIT_LEAVES = ModBlocks.FRUITFUL_LYCHEE_LEAVES.get().defaultBlockState().setValue(LeavesBlock.DISTANCE, 2);
+    private static final WeightedStateProvider LEAVES_STATE_PROVIDER =  new WeightedStateProvider(SimpleWeightedRandomList.<BlockState>builder().add(LEAVES, 3).add(FRUIT_LEAVES, 1));
 
     public LycheeTreeSmall(Codec<NoneFeatureConfiguration> pCodec) {
         super(pCodec);
@@ -32,8 +35,7 @@ public class LycheeTreeSmall extends Feature<NoneFeatureConfiguration> {
         Random random = context.random();
         BlockPos blockPos = pos;
         List<BlockPos> logs = new ArrayList<>();
-        Map<BlockPos, Direction> leaves = new HashMap<>();
-        Map<BlockPos, Direction> fruitLeaves = new HashMap<>();
+        List<BlockPos> leaves = new ArrayList<>();
         int trunkHeight = 3;
 
         if (!TreeUtil.isValidGround(level, pos.below(), (SaplingBlock) ModBlocks.LYCHEE_SAPLING.get())) {
@@ -49,55 +51,33 @@ public class LycheeTreeSmall extends Feature<NoneFeatureConfiguration> {
 
         for (BlockPos log : logs) {
             if (i >= trunkHeight - 1) {
-                for (Direction direction : Direction.Plane.HORIZONTAL.stream().toList()) {
-                    if (i == trunkHeight - 1) {
-                        leaves.put(log.offset(0, 3, 0), direction);
+                for (Direction direction : Direction.values()) {
+                    if (direction.getAxis().isHorizontal()) {
+                        if (i == trunkHeight - 1) {
 
-                        if (random.nextInt(8) != 0) {
-                            leaves.put(log.offset(0, 2, 0), direction);
-                            leaves.put(log.offset(1, 2, 0), direction);
-                            leaves.put(log.offset(0, 2, 1), direction);
-                            leaves.put(log.offset(0, 2, -1), direction);
-                        } else {
-                            fruitLeaves.put(log.offset(0, 2, 0), direction);
-                            fruitLeaves.put(log.offset(1, 2, 0), direction);
-                            fruitLeaves.put(log.offset(0, 2, 1), direction);
-                            fruitLeaves.put(log.offset(0, 2, -1), direction);
-                        }
-                        if (random.nextInt(7) != 0) {
-                            leaves.put(log.relative(direction).above(), direction);
-                            leaves.put(log.offset(0, 1, 0), direction);
-                            leaves.put(log.offset(-1, 1, 1), direction);
-                            leaves.put(log.offset(-1, 1, -1), direction);
-                            leaves.put(log.offset(1, 1, 1), direction);
-                        } else {
-                            fruitLeaves.put(log.relative(direction).above(), direction);
-                            fruitLeaves.put(log.offset(0, 1, 0), direction);
-                            fruitLeaves.put(log.offset(-1, 1, 1), direction);
-                            fruitLeaves.put(log.offset(-1, 1, -1), direction);
-                            fruitLeaves.put(log.offset(1, 1, 1), direction);
-                        }
-                        if (random.nextInt(6) != 0) {
-                            leaves.put(log.offset(-1, 0, 1), direction);
-                            leaves.put(log.offset(1, 0, 1), direction);
-                            leaves.put(log.offset(0, 0, 1), direction);
-                            leaves.put(log.offset(-1, 0, 0), direction);
-                            leaves.put(log.offset(1, 0, -1), direction);
-                            leaves.put(log.offset(0, 0, -1), direction);
-                        } else {
-                            fruitLeaves.put(log.offset(-1, 0, 1), direction);
-                            fruitLeaves.put(log.offset(1, 0, 1), direction);
-                            fruitLeaves.put(log.offset(0, 0, 1), direction);
-                            fruitLeaves.put(log.offset(-1, 0, 0), direction);
-                            fruitLeaves.put(log.offset(1, 0, -1), direction);
-                            fruitLeaves.put(log.offset(0, 0, -1), direction);
-                        }
-                        if (random.nextInt(8) != 0) {
-                            leaves.put(log.offset(0, -1, 1), direction);
-                            leaves.put(log.offset(0, -1, -1), direction);
-                        } else {
-                            fruitLeaves.put(log.offset(0, -1, 1), direction);
-                            fruitLeaves.put(log.offset(0, -1, -1), direction);
+                            leaves.add(log.offset(0, 3, 0));
+
+                            leaves.add(log.offset(0, 2, 0));
+                            leaves.add(log.offset(1, 2, 0));
+                            leaves.add(log.offset(0, 2, 1));
+                            leaves.add(log.offset(0, 2, -1));
+
+                            leaves.add(log.relative(direction).above());
+                            leaves.add(log.offset(0, 1, 0));
+                            leaves.add(log.offset(-1, 1, 1));
+                            leaves.add(log.offset(-1, 1, -1));
+                            leaves.add(log.offset(1, 1, 1));
+
+
+                            leaves.add(log.offset(-1, 0, 1));
+                            leaves.add(log.offset(1, 0, 1));
+                            leaves.add(log.offset(0, 0, 1));
+                            leaves.add(log.offset(-1, 0, 0));
+                            leaves.add(log.offset(1, 0, -1));
+                            leaves.add(log.offset(0, 0, -1));
+
+                            leaves.add(log.offset(0, -1, 1));
+                            leaves.add(log.offset(0, -1, -1));
                         }
                     }
                 }
@@ -111,11 +91,8 @@ public class LycheeTreeSmall extends Feature<NoneFeatureConfiguration> {
             for (BlockPos blockPos1 : logs) {
                 TreeUtil.setForcedState(level, blockPos1, LOG);
             }
-            for (BlockPos blockPos1 : leaves.keySet()) {
-                TreeUtil.setForcedState(level, blockPos1, LEAVES);
-            }
-            for (BlockPos blockPos1 : fruitLeaves.keySet()) {
-                TreeUtil.setForcedState(level, blockPos1, FRUIT_LEAVES);
+            for (BlockPos blockPos1 : leaves) {
+                TreeUtil.setForcedState(level, blockPos1, LEAVES_STATE_PROVIDER.getState(random, blockPos1));
             }
 
             TreeUtil.setDirtAt(level, pos.below());
